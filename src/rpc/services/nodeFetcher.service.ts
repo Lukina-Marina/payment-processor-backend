09090e780cb00337aecf9b22786d1e149f068a15
Lukina-Marina <lukina_marina12@mail.ru>
2024-04-23 11:56:20 +0200

    Deleted RequestInfo.interface and added function call and function getBalance

--- src/rpc/services/nodeFetcher.service.ts
+++ src/rpc/services/nodeFetcher.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
-import { ethers } from "ethers";
+import { ethers, TransactionRequest } from "ethers";
 
 import { config } from "../../../config";
-
-import { RequestInfo, RequestType } from "./interfaces/RequestInfo.interface";
+import { SECOND } from "../../../constants";
+import { sleep } from "../../../helpers";
 
 @Injectable()
 export class NodeFetcherService {
@@ -12,11 +12,53 @@ export class NodeFetcherService {
 
     rpcEndpoint: string;
 
-    requests: RequestInfo[];
-
     constructor() {
         this.rpcEndpoint = config.rpcEndpoint;
 
         this.provider = new ethers.InfuraProvider(this.rpcEndpoint);
+    }
+
+    async call(
+        tx: TransactionRequest,
+        decodeTypes: string[] | undefined = [],
+    ): Promise<any> {
+        const result = await this.callPrivate(tx);
+
+        if (decodeTypes.length > 0) {
+            return new ethers.AbiCoder().decode(decodeTypes, result);
+        }
+
+        return result;
+    }
+
+    async getBalance(address: string, blockNumber: number): Promise<bigint> {
+        return this.getBalancePrivate(address, blockNumber);
+    }
+
+    private async callPrivate(tx: TransactionRequest): Promise<string> {
+        try {
+            return await this.provider.call(tx);
+        } catch (error) {
+            console.log('Error while call:', error.message);
+
+            await sleep(SECOND);
+
+            return this.callPrivate(tx);
+        }
+    }
+
+    private async getBalancePrivate(
+        address: string,
+        blockNumber: number,
+    ): Promise<bigint> {
+        try {
+            return await this.provider.getBalance(address, blockNumber);
+        } catch (error) {
+            console.log('Error while getBalance:', error.message);
+
+            await sleep(SECOND);
+
+            return this.getBalancePrivate(address, blockNumber);
+        }
     }
 }
