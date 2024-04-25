1c89b336438dc77d7880a1bd97daa653375b346f
Lukina-Marina <lukina_marina12@mail.ru>
2024-04-25 13:00:01 +0200

    Added constructor to ContractMonitoringService

--- src/contractMonitoring/services/contractMonitoring.service.ts
+++ src/contractMonitoring/services/contractMonitoring.service.ts
@@ -1,6 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { config } from "../../../config";
+import {
+    UserManagerEventProcessor,
+    SubscriptionManagerEventProcessor,
+    IBaseEventProcessor
+} from './eventProcessors/index';
+import { NodeFetcherService } from "../../rpc/services/nodeFetcher.service";
 
 @Injectable()
 export class ContractMonitoringService {
-    
+    private monitoringParameters: {
+        contractAddress: string,
+        startBlockNumber: number,
+        eventProcessor: IBaseEventProcessor
+    }[];
+
+    constructor(private nodeFetcherService: NodeFetcherService) {
+        this.monitoringParameters = [
+            {
+                contractAddress: config.contracts.subscriptionManager.address,
+                startBlockNumber: config.contracts.subscriptionManager.deployBlockNumber,
+                eventProcessor: new SubscriptionManagerEventProcessor()
+            },
+            {
+                contractAddress: config.contracts.userManager.address,
+                startBlockNumber: config.contracts.userManager.deployBlockNumber,
+                eventProcessor: new UserManagerEventProcessor()
+            }
+        ];
+    }
 }
