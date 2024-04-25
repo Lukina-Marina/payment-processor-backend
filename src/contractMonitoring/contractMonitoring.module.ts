6935fe61c31ac817e3f1bdba4919a9277329cd3e
Lukina-Marina <lukina_marina12@mail.ru>
2024-04-25 12:58:25 +0200

    Imported RpcModule to contractMonitoring.module

--- src/contractMonitoring/contractMonitoring.module.ts
+++ src/contractMonitoring/contractMonitoring.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { RpcModule } from '../rpc/rpc.module';
 import { ContractMonitoringService } from './services/contractMonitoring.service';
 
 @Module({
   providers: [ContractMonitoringService],
+  imports: [RpcModule]
 })
 export class ContractMonitoringModule {}
