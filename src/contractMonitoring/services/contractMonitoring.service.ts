a2e9596172ca04e7c7d50366009a8a3929ca0726
Lukina-Marina <lukina_marina12@mail.ru>
2024-04-29 16:21:54 +0200

    Added part of the startMonitoring function + initialization of the class

--- src/contractMonitoring/services/contractMonitoring.service.ts
+++ src/contractMonitoring/services/contractMonitoring.service.ts
@@ -1,34 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { config } from "../../../config";
 import {
     UserManagerEventProcessor,
     SubscriptionManagerEventProcessor,
-    IBaseEventProcessor
+    BaseEventProcessor
 } from './eventProcessors/index';
 import { NodeFetcherService } from "../../rpc/services/nodeFetcher.service";
 import { DatabaseService } from 'src/database/database.service';
-import { Contract } from "@prisma/client";
 
 @Injectable()
-export class ContractMonitoringService {
+export class ContractMonitoringService implements OnApplicationBootstrap {
     private monitoringParameters: {
         contractAddress: string,
-        startBlockNumber: number,
-        eventProcessor: IBaseEventProcessor
+        startBlockNumber: bigint,
+        eventProcessor: BaseEventProcessor
     }[];
 
     constructor(private nodeFetcherService: NodeFetcherService, private databaseService: DatabaseService) {
         this.monitoringParameters = [
             {
                 contractAddress: config.contracts.subscriptionManager.address,
-                startBlockNumber: config.contracts.subscriptionManager.deployBlockNumber,
+                startBlockNumber: BigInt(config.contracts.subscriptionManager.deployBlockNumber),
                 eventProcessor: new SubscriptionManagerEventProcessor()
             },
             {
                 contractAddress: config.contracts.userManager.address,
-                startBlockNumber: config.contracts.userManager.deployBlockNumber,
+                startBlockNumber: BigInt(config.contracts.userManager.deployBlockNumber),
                 eventProcessor: new UserManagerEventProcessor()
             }
         ];
+    }
+
+    async onApplicationBootstrap(): Promise<void> {
+        for(const monitoringParameter of this.monitoringParameters) {
+            const contractInfo = await this.databaseService.contract.findUnique({
+                where: {
+                    address: monitoringParameter.contractAddress
+                }
+            });
+            if (contractInfo === null) {
+                continue;
+            }
+            
+            if (contractInfo.lastProcessedBlock + 1n > monitoringParameter.startBlockNumber) {
+                monitoringParameter.startBlockNumber = contractInfo.lastProcessedBlock + 1n;
+            }
+        }
+    }
+
+    async startMonitoring(): Promise<void> {
+        while(true) {
+            const latestBlock = await this.nodeFetcherService.getBlock("latest");
+
+            for(const monitoringParameter of this.monitoringParameters) {
+                for(let startBlock = monitoringParameter.startBlockNumber; startBlock <= latestBlock.number; startBlock += BigInt(config.eventPeriod) + 1n) {
+                    const logs = this.nodeFetcherService.getLogs(monitoringParameter.contractAddress, null, startBlock, startBlock + BigInt(config.eventPeriod));
+                    // TODO process events
+                }
+            }
+        }
     }
 }
