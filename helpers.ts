0c07aa96ae7a14442594c3d78783618bce85e00e
Lukina-Marina <lukina_marina12@mail.ru>
2024-05-09 12:11:14 +0200

    Added function min

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,11 @@
 export async function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export function min(x: bigint, y: bigint): bigint {
+  if (x < y) {
+    return x;
+  } else {
+    return y;
+  }
+}
