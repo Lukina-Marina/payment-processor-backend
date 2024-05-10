b25a4e122dc53465dc28e510c2b3f4e69d2bca78
Lukina-Marina <lukina_marina12@mail.ru>
2024-05-10 13:18:16 +0200

    Changed BaseEventProcessor and added functionality to UserManagerEventProcessor

--- src/contractMonitoring/services/eventProcessors/BaseEventProcessor.ts
+++ src/contractMonitoring/services/eventProcessors/BaseEventProcessor.ts
@@ -1,5 +1,27 @@
-import { Log } from "ethers";
+import { Interface, Log, LogDescription } from 'ethers';
+import { DatabaseService } from 'src/database/Database.service';
 
-export class BaseEventProcessor {
-    async processLogs(logs: Log[]) {}
+export abstract class BaseEventProcessor {
+  public contractInterface: Interface;
+
+  constructor(
+    public databaseService: DatabaseService,
+    abi: any,
+  ) {
+    this.contractInterface = new Interface(abi);
+  }
+
+  async processEvents(logs: Log[]): Promise<void> {
+    for (const log of logs) {
+      const parsedLog = this.contractInterface.parseLog(log);
+
+      if (parsedLog === null) {
+        continue;
+      }
+
+      this.processEvent(parsedLog, log);
+    }
+  }
+
+  abstract processEvent(parsedLog: LogDescription, log: Log): Promise<void>;
 }
--- src/contractMonitoring/services/eventProcessors/SubscriptionManagerEventProcessor.ts
+++ src/contractMonitoring/services/eventProcessors/SubscriptionManagerEventProcessor.ts
@@ -1,5 +1,14 @@
+import { DatabaseService } from "src/database/database.service";
 import { BaseEventProcessor } from "./BaseEventProcessor";
+import * as SubscriptionManagerABI from "../../../../abi/SubscriptionManager.json";
+import { Log, LogDescription } from "ethers";
 
 export class SubscriptionManagerEventProcessor extends BaseEventProcessor {
-    
+    constructor(
+        public databaseService: DatabaseService,
+    ) {
+        super(databaseService, SubscriptionManagerABI);
+    }
+
+    async processEvent(parsedLog: LogDescription, log: Log): Promise<void> {}
 }
