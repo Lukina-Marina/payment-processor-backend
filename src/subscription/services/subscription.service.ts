d3b262e3badfac761420e2b8768c46699a908651
Lukina-Marina <lukina_marina12@mail.ru>
2024-05-21 12:16:55 +0200

    Added checks of allowanceOfUser and balanceOfUser

--- src/subscription/services/subscription.service.ts
+++ src/subscription/services/subscription.service.ts
@@ -3,14 +3,18 @@ import { config } from '../../../config';
 import { NodeFetcherService } from '../../rpc/services/nodeFetcher.service';
 import { DatabaseService } from 'src/database/database.service';
 import { SignerService } from 'src/signer/services/signer.service';
-import { Interface } from 'ethers';
+import { FunctionFragment, Interface } from 'ethers';
 import * as Erc20ABI from '../../../abi/ERC20.json';
 import * as UserManagerABI from '../../../abi/UserManager.json';
 
 @Injectable()
 export class SubscriptionService implements OnApplicationBootstrap {
   private monirotringPromise: Promise<void>;
+
   private erc20Interface: Interface;
+  private balanceOfFragment: FunctionFragment;
+  private allowanceFragment: FunctionFragment;
+
   private userManagerInterface: Interface;
 
   constructor(
@@ -19,6 +23,9 @@ export class SubscriptionService implements OnApplicationBootstrap {
     private signerService: SignerService,
   ) {
     this.erc20Interface = new Interface(Erc20ABI);
+    this.balanceOfFragment = this.erc20Interface.getFunction("balanceOf");
+    this.allowanceFragment = this.erc20Interface.getFunction("allowance");
+
     this.userManagerInterface = new Interface(UserManagerABI);
   }
 
@@ -40,6 +47,21 @@ export class SubscriptionService implements OnApplicationBootstrap {
         });
 
       for (const activeSubscription of activeSubscriptions) {
+        const balanceOfData = 
+          await this.erc20Interface.encodeFunctionData(this.balanceOfFragment, [activeSubscription.user]);
+
+        const allowanceData = 
+          await this.erc20Interface.encodeFunctionData(this.allowanceFragment, [activeSubscription.user, config.contracts.userManager]);
+
+        const balanceOfUserRequest = this.nodeFetcherService.call({
+          to: activeSubscription.token,
+          data: balanceOfData
+        });
+        const allowanceOfUserRequest = this.nodeFetcherService.call({
+          to: activeSubscription.token,
+          data: allowanceData
+        });
+
         const subscriptionInfo = await this.databaseService.subscription.findUnique({
           where: {
             appIdAndSubscriptionId: {
@@ -48,8 +70,38 @@ export class SubscriptionService implements OnApplicationBootstrap {
             }
           }
         });
-        //1. проверить баланс
-        //2. проверить апрув
+
+        if (subscriptionInfo.isPaused) {
+          continue;
+        }
+
+        let tokenIndex = -1;
+        for(let i = 0; i < subscriptionInfo.tokens.length; ++i) {
+          if (activeSubscription.token == subscriptionInfo.tokens[i]) {
+            tokenIndex = i;
+            break;
+          }
+        }
+        if (tokenIndex == -1) {
+          continue;
+        }
+        const amount = BigInt(subscriptionInfo.amounts[tokenIndex]);
+
+        const balanceOfUserEncoded = await balanceOfUserRequest;
+        const allowanceOfUserEncoded = await allowanceOfUserRequest;
+
+        const balanceOfUser: bigint = this.erc20Interface.parseCallResult(balanceOfUserEncoded)[0];
+        const allowanceOfUser: bigint = this.erc20Interface.parseCallResult(allowanceOfUserEncoded)[0];
+
+        if (balanceOfUser < amount) {
+          continue;
+        }
+
+        if (allowanceOfUser < amount) {
+          continue;
+        }
+        
+        // TODO
         //3. проверить статус транзакции
         //3.1 если fail, то отправить заново
         //3.2 если success, то удалить статус транзакции
